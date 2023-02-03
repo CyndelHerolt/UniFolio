f3e95fd0370d97f0714dc0d5bcf437f2fa16e733
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-03 11:25:02 +0100

    feat: [Connexion] Génération du formulaire de connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -25,4 +25,10 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
 }
