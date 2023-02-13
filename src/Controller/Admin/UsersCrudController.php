bd6183d84f9a6b6846754da564af6d76855beeb7
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-13 16:10:20 +0100

    feat: [Fix] Création manuelle de l'input password pour cryptage

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -4,16 +4,27 @@ namespace App\Controller\Admin;
 
 use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\{FormBuilderInterface, FormEvent, FormEvents};
 
 class UsersCrudController extends AbstractCrudController
 {
+    public function __construct(
+        public UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Users::class;
@@ -21,14 +32,64 @@ class UsersCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+
+        $password = TextField::new('password')
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => '(Repeat)'],
+                'mapped' => false,
+            ])
+            ->setRequired($pageName === Crud::PAGE_NEW)
+            ->onlyOnForms();
+
         return [
-            ArrayField::new('roles', 'Rôles'),
-            TextField::new('username', 'Nom d\'utilisateur')->hideOnForm(),
-//            TextField::new('prenom'),
+            TextField::new('username', 'Nom d\'utilisateur'),
+            ChoiceField::new('roles', 'Rôles')->setHelp('Rôles disponibles: ROLE_ADMIN, ROLE_ETUDIANT, ROLE_ENSEIGNANT')->allowMultipleChoices(true)->setChoices([
+                'etudiant' => 'ROLE_ETUDIANT',
+                'enseignant' => 'ROLE_ENSEIGNANT',
+                'admin' => 'ROLE_ADMIN',
+            ]),
+            $password,
+//            TextField::new('prenom')->hideOnForm(),
 //            TextField::new('nom'),
 //            EmailField::new('mail'),
 //            EmailField::new('mail_univ'),
         ];
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword() {
+        return function($event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+            $password = $form->get('password')->getData();
+            if ($password === null) {
+                return;
+            }
+
+            $hash = $this->userPasswordHasher->hashPassword($this->getUser(), $password);
+            $form->getData()->setPassword($hash);
+        };
     }
 
     public function configureCrud(Crud $crud): Crud
