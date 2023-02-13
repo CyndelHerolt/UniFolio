c5a0acabbe79c88bf1080bc8121f3df493e1c80c
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-13 16:11:02 +0100

    feat: [Feat] Génération de la page de Profil

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil', name: 'app_profil')]
+    public function index(): Response
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+}
