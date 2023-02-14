b61d2dd21ad252ea5929f88c9d5c9ef645b57a80
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-14 16:29:55 +0100

    add

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
