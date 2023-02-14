8b348d2e9468d2d1229876fc02b362793330f8c0
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-14 16:24:56 +0100

    feat: [Déconnexion] Système de logout depuis le Dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
         if ($this->isGranted('ROLE_ETUDIANT')) {
@@ -16,6 +16,9 @@ class DashboardController extends AbstractController
         }
         elseif ($this->isGranted('ROLE_ENSEIGNANT')) {
             return $this->redirectToRoute('enseignant_dashboard');
+        }
+        elseif ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin');
         }
         else {
             return $this->redirectToRoute('app_login');
