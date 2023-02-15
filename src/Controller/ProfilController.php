69e766d109af0c82e9866d0f4fa847814b8a5282
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-15 16:23:38 +0100

    feat: [Profil] Lien avec la base de données pour récupérer les informations de l'utilisateur

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Enseignant;
+use App\Entity\Etudiant;
+use App\Entity\Users;
+use App\Repository\EtudiantRepository;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProfilController extends AbstractController
 {
+
+    //todo: mode édition
+
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function profil(UsersRepository $usersRepository, EtudiantRepository $etudiantRepository): Response
     {
+        $user = $usersRepository->findOneBy(['username' => $this->getUser()->getUserIdentifier()]);
+        $userId = $this->getUser()->getUserIdentifier();
+//        $role = implode($user->getRoles());
+
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $userId,
         ]);
     }
 }
