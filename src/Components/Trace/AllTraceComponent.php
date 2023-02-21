43ba8535ec61b9841700471a560e29d7161f302e
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-21 15:52:20 +0100

    feat: [Traces] Récupération et affichage sommaire de la bibliothèque de traces de l'utilisateur connecté

--- src/Components/Trace/AllTraceComponent.php
+++ src/Components/Trace/AllTraceComponent.php
@@ -2,17 +2,28 @@
 namespace App\Components\Trace;
 
 use App\Entity\Trace;
+use App\Repository\BibliothequeRepository;
 use App\Repository\TraceRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Contracts\Service\Attribute\Required;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('all_trace')]
 class AllTraceComponent {
 
-	public function __construct(private TraceRepository $traceRepository)
+	public function __construct(
+        public TraceRepository $traceRepository,
+        public BibliothequeRepository $bibliothequeRepository,
+        #[Required] public Security $security
+    )
     {}
 
-	public function getAllTrace(): array
+    public function getAllTrace(): array
     {
-        return $this->traceRepository->findAll();
+        // Récupérer la bibliothèque de l'utilisateur connecté
+        $etudiant = $this->security->getUser()->getEtudiant();
+        $biblio = $this->bibliothequeRepository->findOneBy(['etudiant' => $etudiant]);
+        // Récupérer les traces de la bibliothèque
+        return $this->traceRepository->findBy(['bibliotheque' => $biblio]);
     }
 }
