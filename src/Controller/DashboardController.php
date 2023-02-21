4ca9260feef16ba07bec7480390bbb64330ebeff
CyndelHerolt <herolt.cyndel@gmail.com>
2023-02-21 15:58:50 +0100

    feat: [Dashboard] Structure contenu du dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TraceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,7 @@ class DashboardController extends AbstractController
     public function index(): Response
     {
         if ($this->isGranted('ROLE_ETUDIANT')) {
+
             return $this->redirectToRoute('etudiant_dashboard');
         }
         elseif ($this->isGranted('ROLE_ENSEIGNANT')) {
