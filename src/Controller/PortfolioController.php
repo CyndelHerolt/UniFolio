60c2aba3978d5e717fb04466c1a74c9e909820a0
CyndelHerolt <herolt.cyndel@gmail.com>
2023-03-06 12:02:34 +0100

    feat: [Portfolio] Ajout d'une page directement depuis la bibliothèque

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -10,7 +10,6 @@ use App\Repository\PortfolioRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
