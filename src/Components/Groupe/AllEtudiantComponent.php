e63d569d708b8bd64ad6bb16212f9dd244e099f2
CyndelHerolt <herolt.cyndel@gmail.com>
2023-03-27 15:32:47 +0200

    feat: [Enseignant] Génération de la structure "en dur" de la gestion des étudiants suivis sur le dashboard enseignant

---
+++ src/Components/Groupe/AllEtudiantComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Components\Groupe;
+
+use App\Repository\UsersRepository;
+use App\Repository\GroupeRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+
+#AsTwigComponent('all_etudiant')
+class AllEtudiantComponent
+{
+//TODO: Implementer AllEtudiantComponent avec les étudiants et leur groupe (détails : nom, prénom, groupeTP, lien vers dernier retour pédagogique)
+}
---
+++ src/Components/Groupe/EtudiantTableComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Components\Groupe;
+
+use App\Repository\UsersRepository;
+use App\Repository\GroupeRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('etudiant_table')]
+class EtudiantTableComponent
+{
+ //TODO: Implementer EtudiantTableComponent avec les étudiants et leur groupe (détails : nom, prénom, groupeTP, lien vers dernier retour pédagogique)
+}
