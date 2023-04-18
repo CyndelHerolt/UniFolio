e8e6ecf4c9cb74079d3c9de02d435e06a2fffe9d
CyndelHerolt <herolt.cyndel@gmail.com>
2023-04-18 14:20:01 +0200

    feat: [Data] Récupération des départements depuis l'intranet

--- src/Controller/SynchroIntranet/SynchroIntranetController.php
+++ src/Controller/SynchroIntranet/SynchroIntranetController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\SynchroIntranet;
 
+use App\Entity\Departement;
+use App\Entity\Semestre;
+use App\Repository\DepartementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +13,17 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class SynchroIntranetController extends AbstractController
 {
     #[Route('/test/requete', name: 'app_synchro_intranet')]
-    public function index(HttpClientInterface $client): Response
+    public function index(
+        HttpClientInterface   $client,
+        DepartementRepository $departementRepository
+    ): Response
     {
         //https://symfony.com/doc/current/http_client.html
 
         //todo: il faut modifier services.yaml pour ajouter la clé API récupérée depuis le fichier .env.local
         //todo: en local il faut désactiver la vérification du certificat SSL => framewrok.yaml
 
-        $response = $client->request(
+        $departements = $client->request(
             'GET',
             'http://127.0.0.1:8001/fr/api/unifolio/departement/liste',
             [
@@ -29,11 +35,37 @@ class SynchroIntranetController extends AbstractController
             ]
         );
 
-        dump($response->getContent());
-        die();
+        $departements = $departements->toArray();
+        foreach ($departements as $departement) {
+            $existingDepartement = $departementRepository->findOneBy(['libelle' => $departement['libelle']]);
+            //Vérifier si le libelle du département existe déjà en base de données
+            if ($existingDepartement) {
+                $existingDepartement->setLibelle($departement['libelle']);
+                $existingDepartement->setLogoName($departement['logo']);
+                $existingDepartement->setCouleur($departement['couleur']);
+                $existingDepartement->setTelContact($departement['tel_contact']);
+                $existingDepartement->setDescription($departement['description']);
+                $departementRepository->save($existingDepartement, true);
+                } else {
+                //Sinon, on le crée
+                $newDepartement = new Departement();
+                $newDepartement->setLibelle($departement['libelle']);
+                $newDepartement->setLogoName($departement['logo']);
+                $newDepartement->setCouleur($departement['couleur']);
+                $newDepartement->setTelContact($departement['tel_contact']);
+                $newDepartement->setDescription($departement['description']);
+                $departementRepository->save($newDepartement, true);
+            }
+        }
+        $this->addFlash('success', 'Les données ont bien été importées.');
 
-        return $this->redirectToRoute('app_dashboard', [
-            'reponses' => $response->toArray()
+
+//        dump($response->getContent());
+//        die();
+
+
+        return $this->render('synchro_intranet/index.html.twig', [
+            'departements' => $departements
         ]);
     }
 }
