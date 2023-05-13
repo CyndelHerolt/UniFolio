e5206e3d3694984559f809a797490c5f84d475dc
CyndelHerolt <herolt.cyndel@gmail.com>
2023-05-13 10:35:02 +0200

    fix: [Filter] Filtre semestres/annees

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -10,37 +10,5 @@ use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
-//    private $dataUserSession;
-//
-//    public function __construct(DataUserSession $dataUserSession, RequestStack $session)
-//    {
-//        $this->dataUserSession = $dataUserSession;
-//        $this->session = $session;
-//    }
-//
-//    public function getFilters()
-//    {
-//        return [
-//            new TwigFilter('filtre', [$this, 'filtreSemestres']),
-//        ];
-//    }
-//
-//    public function filtreSemestres($semestresActifs, $ordre)
-//    {
-//        $this->session->getSession();
-//        $annee = null;
-//        $annees = $this->dataUserSession->getAnnees();
-//
-//        foreach ($annees as $anneeItem) {
-//            if ($anneeItem->getOrdre() === $ordre) {
-//                $annee = $anneeItem;
-//            }
-//        }
-//
-//        if ($annee === null) {
-//            return [];
-//        }
-//
-//        return $this->dataUserSession->getSemestresByAnnee($annee, $ordre);
-//    }
+
 }
