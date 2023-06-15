bb7d47d029d4c7b63f12b6fe60071a403e883be7
CyndelHerolt <herolt.cyndel@gmail.com>
2023-06-15 13:04:32 +0200

    feat : [Admin] Création d'un objet Etudiant + son objet User

--- src/Controller/Admin/EtudiantCrudController.php
+++ src/Controller/Admin/EtudiantCrudController.php
@@ -4,19 +4,35 @@ namespace App\Controller\Admin;
 
 use App\Entity\Etudiant;
 use App\Entity\Users;
-use App\Form\UsersType;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterCrudActionEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeCrudActionEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Exception\ForbiddenActionException;
+use EasyCorp\Bundle\EasyAdminBundle\Exception\InsufficientEntityPermissionException;
+use EasyCorp\Bundle\EasyAdminBundle\Factory\EntityFactory;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Faker\Provider\Text;
+use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\HttpFoundation\Request;
 
 class EtudiantCrudController extends AbstractCrudController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     public static function getEntityFqcn(): string
     {
@@ -38,15 +54,19 @@ class EtudiantCrudController extends AbstractCrudController
         yield TextField::new('telephone', 'Téléphone');
         yield TextField::new('bio', 'Bio');
 
-        //todo: ajouter les champs de l'utilisateur et gérer l'envoi en db
-
-        yield FormField::addPanel('Informations Utilisateur')->setIcon('fa fa-user');
-        yield 'user' => FormField::addPanel('Utilisateur')
-            ->setFormType(UsersType::class)
-            ->setFormTypeOptions([
+//        yield FormField::addPanel('Informations Utilisateur')->setIcon('fa fa-user');
+//        yield 'user' => FormField::addPanel('Utilisateur')
+//            ->setFormType(UsersType::class)
+//            ->setFormTypeOptions([
 //                'required' => 'true',
-            ]);
-        // Ajoutez le formulaire UsersType ici
+//            ]);
+
+        yield TextField::new('username')
+            // mapped false pour ne pas envoyer en db
+            ->setFormTypeOption('mapped', false);
+        yield TextField::new('password')
+            ->setFormTypeOption('mapped', false);
+
         yield ChoiceField::new('roles')
             ->setChoices(['ROLE_ETUDIANT' => 'ROLE_ETUDIANT', 'ROLE_ADMIN' => 'ROLE_ADMIN'])
             ->allowMultipleChoices()
@@ -70,13 +90,94 @@ class EtudiantCrudController extends AbstractCrudController
             ->setPageTitle('edit', 'Modifier un.e étudiant.e');
     }
 
-    public function createEntity(string $entityFqcn)
-    {
-        $etudiant = new Etudiant();
-        $user = new Users();
-        $etudiant->setUsers($user);
+//    public function createEntity(string $entityFqcn)
+//    {
+//        $etudiant = new Etudiant();
+//        $user = new Users();
+//        $etudiant->setUsers($user);
+//
+//        return $etudiant;
+//    }
 
-        return $etudiant;
+    public function new(AdminContext $context)
+    {
+        $event = new BeforeCrudActionEvent($context);
+        $this->container->get('event_dispatcher')->dispatch($event);
+        if ($event->isPropagationStopped()) {
+            return $event->getResponse();
+        }
+
+        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::NEW, 'entity' => null])) {
+            throw new ForbiddenActionException($context);
+        }
+
+        if (!$context->getEntity()->isAccessible()) {
+            throw new InsufficientEntityPermissionException($context);
+        }
+
+        $context->getEntity()->setInstance($this->createEntity($context->getEntity()->getFqcn()));
+        $this->container->get(EntityFactory::class)->processFields($context->getEntity(), FieldCollection::new($this->configureFields(Crud::PAGE_NEW)));
+        $context->getCrud()->setFieldAssets($this->getFieldAssets($context->getEntity()->getFields()));
+        $this->container->get(EntityFactory::class)->processActions($context->getEntity(), $context->getCrud()->getActionsConfig());
+
+        $newForm = $this->createNewForm($context->getEntity(), $context->getCrud()->getNewFormOptions(), $context);
+        $newForm->handleRequest($context->getRequest());
+
+        $entityInstance = $newForm->getData();
+        $context->getEntity()->setInstance($entityInstance);
+
+        if ($newForm->isSubmitted() && $newForm->isValid()) {
+
+//            dd($context->getRequest()->request);
+
+            $this->processUploadedFiles($newForm);
+
+            $event = new BeforeEntityPersistedEvent($entityInstance);
+            $this->container->get('event_dispatcher')->dispatch($event);
+            $entityInstance = $event->getEntityInstance();
+
+            $user = new Users();
+            $user->setUsername($context->getRequest()->request->all('Etudiant')['username']);
+            $user->setPassword($context->getRequest()->request->all('Etudiant')['password']);
+            $user->setRoles($context->getRequest()->request->all('Etudiant')['roles']);
+
+            $etudiant = new Etudiant();
+            $etudiant->setUsername($context->getRequest()->request->all('Etudiant')['username']);
+            $etudiant->setPrenom($context->getRequest()->request->all('Etudiant')['prenom']);
+            $etudiant->setNom($context->getRequest()->request->all('Etudiant')['nom']);
+            $etudiant->setMailPerso($context->getRequest()->request->all('Etudiant')['mail_perso']);
+            $etudiant->setMailUniv($context->getRequest()->request->all('Etudiant')['mail_univ']);
+            $etudiant->setTelephone($context->getRequest()->request->all('Etudiant')['telephone']);
+            $etudiant->setBio($context->getRequest()->request->all('Etudiant')['bio']);
+
+            $user->setEtudiant($etudiant);
+
+            $usersRepository = $this->entityManager->getRepository(Users::class);
+            $etudiantRepository = $this->entityManager->getRepository(Etudiant::class);
+
+            $usersRepository->save($user, true);
+            $etudiantRepository->save($etudiant, true);
+
+            $this->container->get('event_dispatcher')->dispatch(new AfterEntityPersistedEvent($entityInstance));
+            $context->getEntity()->setInstance($entityInstance);
+
+            return $this->getRedirectResponseAfterSave($context, Action::NEW);
+        }
+
+        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
+            'pageName' => Crud::PAGE_NEW,
+            'templateName' => 'crud/new',
+            'entity' => $context->getEntity(),
+            'new_form' => $newForm,
+        ]));
+
+        $event = new AfterCrudActionEvent($context, $responseParameters);
+        $this->container->get('event_dispatcher')->dispatch($event);
+        if ($event->isPropagationStopped()) {
+            return $event->getResponse();
+        }
+
+        return $responseParameters;
     }
 
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
