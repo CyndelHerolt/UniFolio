7cc146c155ca834d9328248408d309f71c69d787
CyndelHerolt <herolt.cyndel@gmail.com>
2023-08-08 12:57:51 +0200

    feat : [bibliotheque] Tri des resultats par date et % de validation

--- src/Components/Trace/AllTraceComponent.php
+++ src/Components/Trace/AllTraceComponent.php
@@ -27,6 +27,12 @@ class AllTraceComponent extends BaseController
     public array $selectedCompetences = [];
 
     #[LiveProp(writable: true)]
+    public string $selectedOrdreDate = '';
+
+    #[LiveProp(writable: true)]
+    public string $selectedOrdreValidation = '';
+
+    #[LiveProp(writable: true)]
     /** @var Trace[] */
     public array $allTraces = [];
 
@@ -70,7 +76,21 @@ class AllTraceComponent extends BaseController
     #[LiveAction]
     public function changeCompetences()
     {
-        dump($this->selectedCompetences);
+//        dump($this->selectedCompetences);
+        $this->allTraces = $this->getAllTrace();
+    }
+
+    #[LiveAction]
+    public function changeOrdreDate()
+    {
+//        dd($this->selectedOrdreDate);
+        $this->allTraces = $this->getAllTrace();
+    }
+
+    #[LiveAction]
+    public function changeOrdreValidation()
+    {
+//        dd($this->selectedOrdreValidation);
         $this->allTraces = $this->getAllTrace();
     }
 
@@ -84,8 +104,11 @@ class AllTraceComponent extends BaseController
         // Vérifier si la requête n'est pas null (ce sera le cas si le composant est appelé hors d'une requête),
         // puis récupérer le paramètre 'competence' de la requête
         $competence = [];
-        dump(count($this->selectedCompetences));
         $competence = count($this->selectedCompetences) > 0 ? $this->selectedCompetences : null;
+
+        $ordreDate = $this->selectedOrdreDate != null ? $this->selectedOrdreDate : null;
+
+        $ordreValidation = $this->selectedOrdreValidation != null ? $this->selectedOrdreValidation : null;
 
 
         // Si un formulaire en méthode post est reçu
@@ -95,11 +118,66 @@ class AllTraceComponent extends BaseController
                 // On récupère les compétences sélectionnées
                 $traces = $this->traceRepository->findByCompetence($competence);
             } else {
-                // Sinon, on récupère toutes les traces et "msg d'erreur"
-                $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio]);
+                if ($ordreDate !== null) {
+//                dd($ordreDate);
+                    $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio], ['date_modification' => $ordreDate]);
+                } elseif ($ordreValidation !== null) {
+                    // on récupère les traces de la bibliothèque de l'utilisateur connecté
+                    $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio]);
+                    // on récupère les validations des traces
+                    foreach ($traces as $key => $trace) {
+                        $validation[] = $trace->getValidations();
+                    }
+                    // Si $ordreValidation = "ASC", les traces dont les validations ont le plus de propriétés etat == 3 seront en premier
+                    if ($ordreValidation == "ASC") {
+                        // on trie les validations par ordre croissant
+                        sort($validation);
+                        // on récupère les traces dont les validations sont triées
+                        foreach ($validation as $key => $val) {
+                            $traces[] = $val[$key]->getTrace();
+                        }
+                    } else {
+                        // on trie les validations par ordre décroissant
+                        rsort($validation);
+                        // on récupère les traces dont les validations sont triées
+                        foreach ($validation as $key => $val) {
+                            $traces[] = $val[$key]->getTrace();
+                        }
+                    }
+
+                } else {
+                    $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio], ['date_modification' => 'DESC']);
+                }
             }
         } else {
-            $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio]);
+            if ($ordreDate !== null) {
+//                dd($ordreDate);
+                $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio], ['date_modification' => $ordreDate]);
+//                dd($traces);
+            } elseif ($ordreValidation !== null) {
+                // On récupère les traces de la bibliothèque de l'utilisateur connecté
+                $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio]);
+
+                // On trie les traces en fonction du nombre de validations avec état égal à 3
+                usort($traces, function ($a, $b) use ($ordreValidation) {
+                    $validationsA = $a->getValidations()->filter(function ($validation) {
+                        return $validation->isEtat() == 3;
+                    })->count(); // Compter le nombre de validations avec état égal à 3
+
+                    $validationsB = $b->getValidations()->filter(function ($validation) {
+                        return $validation->isEtat() == 3;
+                    })->count(); // Compter le nombre de validations avec état égal à 3
+
+                    // Comparer le nombre de validations avec état égal à 3 en fonction de l'ordre
+                    if ($ordreValidation === "ASC") {
+                        return $validationsA - $validationsB; // Inverser l'ordre pour ASC
+                    } else {
+                        return $validationsB - $validationsA; // Garder l'ordre pour DESC
+                    }
+                });
+            } else {
+                $traces = $this->traceRepository->findBy(['bibliotheque' => $biblio], ['date_modification' => 'DESC']);
+            }
             // retirer les traces qui n'ont pas de type
             foreach ($traces as $key => $trace) {
                 if ($trace->getTypeTrace() == null) {
