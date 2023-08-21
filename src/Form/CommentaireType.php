f04131f77522e937871cf0f838daffbec23920d2
CyndelHerolt <herolt.cyndel@gmail.com>
2023-08-21 11:25:46 +0200

    feat : [Eval] Affichage des commentaires

---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+//            ->add('date_creation')
+//            ->add('date_modification')
+            ->add('contenu', TextareaType::class, [
+                'label'    => 'Ajouter un commentaire',
+                'required' => true,
+                'attr'     => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('visibilite', ChoiceType::class, [
+                'label'    => 'Visibilité',
+                'required' => true,
+                'attr'     => [
+                    'class' => 'switch',
+                ],
+                'choices'  => [
+                    'Public'   => true,
+                    'Privé' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                // cocher par défaut
+                'data'     => true,
+                'mapped'   => true,
+            ])
+//            ->add('portfolio')
+//            ->add('trace')
+//            ->add('enseignant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
