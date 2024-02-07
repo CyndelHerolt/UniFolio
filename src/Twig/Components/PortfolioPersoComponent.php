252735798a134fa76d3b4fa7150bf33c6a2f79ab
CyndelHerolt <herolt.cyndel@gmail.com>
2024-02-07 15:34:59 +0100

    wip: transform Editeur en LiveComponent, manage bloc

--- src/Twig/Components/PortfolioPersoComponent.php
+++ src/Twig/Components/PortfolioPersoComponent.php
@@ -9,7 +9,7 @@ namespace App\Twig\Components;
 
 use App\Components\Editeur\EditeurRegistry;
 use App\Controller\BaseController;
-use App\Entity\PagePerso;
+use App\Entity\Bloc;
 use App\Entity\PortfolioPerso;
 use App\Form\PortfolioPersoType;
 use App\Repository\BlocRepository;
@@ -22,16 +22,12 @@ use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('PortfolioPersoComponent')]
-class PortfolioPersoComponent extends BaseController
+final class PortfolioPersoComponent extends BaseController
 {
     use DefaultActionTrait;
 
     #[LiveProp(writable: true)]
-    public ?PortfolioPerso $portfolio;
-
-    #[LiveProp(writable: true)]
-    /** @var PagePerso[] */
-    public ?array $pages = [];
+    public ?PortfolioPerso $portfolioPerso;
 
     public function __construct(
         protected PortfolioPersoRepository $portfolioPersoRepository,
@@ -40,6 +36,42 @@ class PortfolioPersoComponent extends BaseController
         protected EditeurRegistry          $editeurRegistry,
     )
     {
+    }
+
+
+    #[LiveAction]
+    public function addBloc(#[LiveArg] ?int $pageId): void
+    {
+        $page = $this->pagePersoRepository->find($pageId);
+
+        $bloc = new Bloc();
+        $bloc->setOrdre(count($page->getBlocs()) + 1);
+        $bloc->addPage($page);
+        $this->blocRepository->save($bloc);
+
+        $blocs = $this->blocRepository->findByPage($page);
+        foreach ($blocs as $b) {
+            $page->addBloc($b);
+        }
+
+        $this->portfolioPerso = $this->getPortfolioPerso();
+    }
+
+    #[LiveAction]
+    public function deleteBloc(#[LiveArg] ?int $blocId, #[LiveArg] ?int $pageId): void
+    {
+        $bloc = $this->blocRepository->find($blocId);
+        $page = $this->pagePersoRepository->find($pageId);
+        $ordre = $bloc->getOrdre();
+        $this->blocRepository->remove($bloc);
+        $blocs = $this->blocRepository->findByPage($page);
+        foreach ($blocs as $b) {
+            if ($b->getOrdre() > $ordre) {
+                $b->setOrdre($b->getOrdre() - 1);
+                $this->blocRepository->save($b);
+            }
+        }
+        $this->portfolioPerso = $this->getPortfolioPerso();
     }
 
     #[LiveAction]
@@ -60,7 +92,8 @@ class PortfolioPersoComponent extends BaseController
             $this->blocRepository->save($bloc);
         }
 
-        $this->portfolio = $this->portfolioPersoRepository->findPortfolioByBloc($blocId);
+//        $this->portfolio = $this->portfolioPersoRepository->findPortfolioByBloc($blocId);
+        $this->portfolioPerso = $this->getPortfolioPerso();
     }
 
     #[LiveAction]
@@ -70,11 +103,8 @@ class PortfolioPersoComponent extends BaseController
         $ordre = $bloc->getOrdre();
         $page = $this->pagePersoRepository->find($pageId);
 
-        // Get the bloc with the highest order
-        $highestOrderBloc = $this->blocRepository->findByPage($page, ['ordre' => 'DESC'])[0];
-
         // Check if the bloc is not the last in order
-        if ($ordre < $highestOrderBloc->getOrdre()) {
+        if ($ordre != count($page->getBlocs())) {
             $blocs = $this->blocRepository->findByPage($page);
             foreach ($blocs as $b) {
                 if ($b->getOrdre() == $ordre + 1) {
@@ -85,13 +115,17 @@ class PortfolioPersoComponent extends BaseController
             $bloc->setOrdre($ordre + 1);
             $this->blocRepository->save($bloc);
         }
-
-        $this->portfolio = $this->portfolioPersoRepository->findPortfolioByBloc($blocId);
+        $this->portfolioPerso = $this->getPortfolioPerso();
     }
 
-    public function getPortfolio()
+    public function getPortfolioPerso()
     {
-        $this->pages = $this->pagePersoRepository->findBy(['portfolio' => $this->portfolio], ['ordre' => 'ASC']);
-        $this->form = $this->createForm(PortfolioPersoType::class, $this->portfolio)->createView();
+        $this->form = $this->createForm(PortfolioPersoType::class, $this->portfolioPerso)->createView();
+        $this->elements = $this->editeurRegistry->getElements();
+
+        dump(count($this->portfolioPerso->getPagePersos()[0]->getBlocs()));
+
+        return $this->portfolioPerso;
     }
+
 }
