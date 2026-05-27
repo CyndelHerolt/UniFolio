01acd384715d88babe9f077bca19d4227a1e266b
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2026-05-27 14:07:22 +0200

    fix: easyadmin event subscriber for enseignant CRUD operations

--- src/Controller/Admin/EnseignantCrudController.php
+++ src/Controller/Admin/EnseignantCrudController.php
@@ -1,52 +1,29 @@
 <?php
 
-/*
- * Copyright (c) 2023. | Cyndel Herolt | IUT de Troyes  - All Rights Reserved
- * @author cyndelherolt
- * @project UniFolio
- */
 namespace App\Controller\Admin;
 
 use App\Entity\Enseignant;
 use App\Entity\Users;
 use Doctrine\ORM\EntityManagerInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterCrudActionEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityUpdatedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeCrudActionEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\ForbiddenActionException;
-use EasyCorp\Bundle\EasyAdminBundle\Exception\InsufficientEntityPermissionException;
-use EasyCorp\Bundle\EasyAdminBundle\Factory\EntityFactory;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 
-class EnseignantCrudController extends AbstractCrudController
+class EnseignantCrudController extends AbstractCrudController implements EventSubscriberInterface
 {
-    private $entityManager;
-
     public function __construct(
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher,
     ) {
-        $this->entityManager = $entityManager;
-        $this->passwordHasher = $passwordHasher;
     }
 
     public static function getEntityFqcn(): string
@@ -54,33 +31,90 @@ class EnseignantCrudController extends AbstractCrudController
         return Enseignant::class;
     }
 
+    // +++ Abonnement aux événements EasyAdmin au lieu de surcharger new() et edit()
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => 'onBeforeEntityPersisted',
+            BeforeEntityUpdatedEvent::class => 'onBeforeEntityUpdated',
+        ];
+    }
+
+    public function onBeforeEntityPersisted(BeforeEntityPersistedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Enseignant) {
+            return;
+        }
+
+        // Récupération de la requête courante
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $data = $request->request->all('Enseignant');
+
+        $user = new Users();
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $data['password']);
+
+        $user->setUsername($data['username']);
+        $user->setPassword($hashedPassword);
+        $user->setRoles($data['roles'] ?? []);
+        $user->setIsVerified(true);
+        $user->setEnseignant($entity);
+
+        $this->entityManager->persist($user);
+        // L'enseignant sera persisté automatiquement par EasyAdmin
+    }
+
+    public function onBeforeEntityUpdated(BeforeEntityUpdatedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Enseignant) {
+            return;
+        }
+
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $data = $request->request->all('Enseignant');
+
+        $user = $this->entityManager->getRepository(Users::class)
+            ->findOneBy(['username' => $data['username']]);
+
+        if ($user) {
+            $user->setUsername($data['username']);
+            $user->setRoles($data['roles'] ?? []);
+            $this->entityManager->persist($user);
+        }
+    }
+
     public function configureFields(string $pageName): iterable
     {
-
-        // Enseignant
         yield FormField::addPanel('Informations Enseignant')->setIcon('fa fa-user');
         yield TextField::new('prenom', 'Prénom')
-            ->setFormTypeOption('required', 'true');
+            ->setFormTypeOption('required', true);
         yield TextField::new('nom', 'Nom')
-            ->setFormTypeOption('required', 'true');
+            ->setFormTypeOption('required', true);
         yield TextField::new('mail_perso', 'Mail personnel')
             ->setFormType(EmailType::class);
         yield TextField::new('mail_univ', 'Mail universitaire')
-            ->setFormTypeOption('required', 'true')
+            ->setFormTypeOption('required', true)
             ->setFormType(EmailType::class);
         yield TextField::new('telephone', 'Téléphone');
-
-        //User
         yield TextField::new('username')
-            ->setFormTypeOption('required', 'true');
-        //Si on se trouve sur la page new
-        if ($pageName == Crud::PAGE_NEW) {
+            ->setFormTypeOption('required', true);
+
+        if ($pageName === Crud::PAGE_NEW) {
             yield TextField::new('password')
                 ->setFormTypeOption('mapped', false)
-                ->setFormTypeOption('required', 'true');
+                ->setFormTypeOption('required', true);
         }
+
         yield ChoiceField::new('roles')
-            ->setChoices(['ENSEIGNANT' => 'ROLE_ENSEIGNANT', 'ADMIN' => 'ROLE_ADMIN', 'TEST' => 'ROLE_TEST'])
+            ->setChoices([
+                'ENSEIGNANT' => 'ROLE_ENSEIGNANT',
+                'ADMIN' => 'ROLE_ADMIN',
+                'TEST' => 'ROLE_TEST'
+            ])
             ->allowMultipleChoices()
             ->setRequired(true)
             ->setFormTypeOption('mapped', false)
@@ -102,190 +136,17 @@ class EnseignantCrudController extends AbstractCrudController
             ->setPageTitle('edit', 'Modifier un.e enseignant.e');
     }
 
-    public function new(AdminContext $context): Response|KeyValueStore
-    {
-        $event = new BeforeCrudActionEvent($context);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::NEW, 'entity' => null])) {
-            throw new ForbiddenActionException($context);
-        }
-
-        if (!$context->getEntity()->isAccessible()) {
-            throw new InsufficientEntityPermissionException($context);
-        }
-
-        $context->getEntity()->setInstance($this->createEntity($context->getEntity()->getFqcn()));
-        $this->container->get(EntityFactory::class)->processFields($context->getEntity(), FieldCollection::new($this->configureFields(Crud::PAGE_NEW)));
-        $context->getCrud()->setFieldAssets($this->getFieldAssets($context->getEntity()->getFields()));
-        $this->container->get(EntityFactory::class)->processActions($context->getEntity(), $context->getCrud()->getActionsConfig());
-
-        $newForm = $this->createNewForm($context->getEntity(), $context->getCrud()->getNewFormOptions(), $context);
-        $newForm->handleRequest($context->getRequest());
-
-        $entityInstance = $newForm->getData();
-        $context->getEntity()->setInstance($entityInstance);
-
-        if ($newForm->isSubmitted() && $newForm->isValid()) {
-            $this->processUploadedFiles($newForm);
-
-            $event = new BeforeEntityPersistedEvent($entityInstance);
-            $this->container->get('event_dispatcher')->dispatch($event);
-            $entityInstance = $event->getEntityInstance();
-
-            $user = new Users();
-
-            // password hashing
-            $plaintextPassword = $context->getRequest()->request->all('Enseignant')['password'];
-
-            // hash the password (based on the security.yaml config for the $user class)
-            $hashedPassword = $this->passwordHasher->hashPassword(
-                $user,
-                $plaintextPassword
-            );
-
-
-            $user->setUsername($context->getRequest()->request->all('Enseignant')['username']);
-            $user->setPassword($hashedPassword);
-            $user->setRoles($context->getRequest()->request->all('Enseignant')['roles']);
-            $user->setIsVerified(true);
-
-            $enseignant = new Enseignant();
-            $enseignant->setUsername($context->getRequest()->request->all('Enseignant')['username']);
-            $enseignant->setPrenom($context->getRequest()->request->all('Enseignant')['prenom']);
-            $enseignant->setNom($context->getRequest()->request->all('Enseignant')['nom']);
-            $enseignant->setMailPerso($context->getRequest()->request->all('Enseignant')['mail_perso']);
-            $enseignant->setMailUniv($context->getRequest()->request->all('Enseignant')['mail_univ']);
-            $enseignant->setTelephone($context->getRequest()->request->all('Enseignant')['telephone']);
-
-            $user->setEnseignant($enseignant);
-
-            $usersRepository = $this->entityManager->getRepository(Users::class);
-            $enseignantRepository = $this->entityManager->getRepository(Enseignant::class);
-
-            $usersRepository->save($user, true);
-            $enseignantRepository->save($enseignant, true);
-
-            $this->container->get('event_dispatcher')->dispatch(new AfterEntityPersistedEvent($entityInstance));
-            $context->getEntity()->setInstance($entityInstance);
-
-            return $this->getRedirectResponseAfterSave($context, Action::NEW);
-        }
-
-        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
-            'pageName' => Crud::PAGE_NEW,
-            'templateName' => 'crud/new',
-            'entity' => $context->getEntity(),
-            'new_form' => $newForm,
-        ]));
-
-        $event = new AfterCrudActionEvent($context, $responseParameters);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        return $responseParameters;
-    }
-
-    public function edit(AdminContext $context): Response|KeyValueStore
-    {
-        $event = new BeforeCrudActionEvent($context);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        if (!$this->isGranted(Permission::EA_EXECUTE_ACTION, ['action' => Action::EDIT, 'entity' => $context->getEntity()])) {
-            throw new ForbiddenActionException($context);
-        }
-
-        if (!$context->getEntity()->isAccessible()) {
-            throw new InsufficientEntityPermissionException($context);
-        }
-
-        $this->container->get(EntityFactory::class)->processFields($context->getEntity(), FieldCollection::new($this->configureFields(Crud::PAGE_EDIT)));
-        $context->getCrud()->setFieldAssets($this->getFieldAssets($context->getEntity()->getFields()));
-        $this->container->get(EntityFactory::class)->processActions($context->getEntity(), $context->getCrud()->getActionsConfig());
-        $entityInstance = $context->getEntity()->getInstance();
-
-        if ($context->getRequest()->isXmlHttpRequest()) {
-            if ('PATCH' !== $context->getRequest()->getMethod()) {
-                throw new MethodNotAllowedHttpException(['PATCH']);
-            }
-
-            if (!$this->isCsrfTokenValid(BooleanField::CSRF_TOKEN_NAME, $context->getRequest()->query->get('csrfToken'))) {
-                if (class_exists(InvalidCsrfTokenException::class)) {
-                    throw new InvalidCsrfTokenException();
-                } else {
-                    return new Response('Invalid CSRF token.', 400);
-                }
-            }
-
-            $fieldName = $context->getRequest()->query->get('fieldName');
-            $newValue = 'true' === mb_strtolower($context->getRequest()->query->get('newValue'));
-
-            try {
-                $event = $this->ajaxEdit($context->getEntity(), $fieldName, $newValue);
-            } catch (\Exception) {
-                throw new BadRequestHttpException();
-            }
-
-            if ($event->isPropagationStopped()) {
-                return $event->getResponse();
-            }
-
-            return new Response($newValue ? '1' : '0');
-        }
-
-
-        $editForm = $this->createEditForm($context->getEntity(), $context->getCrud()->getEditFormOptions(), $context);
-        $editForm->handleRequest($context->getRequest());
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-//            dd($context->getRequest()->request->all('Enseignant')['roles']);
-
-            $this->processUploadedFiles($editForm);
-
-            $event = new BeforeEntityUpdatedEvent($entityInstance);
-            $this->container->get('event_dispatcher')->dispatch($event);
-            $entityInstance = $event->getEntityInstance();
-
-            $user = $this->entityManager->getRepository(Users::class)->findOneBy(['username' => $context->getRequest()->request->all('Enseignant')['username']]);
-
-            $user->setUsername($context->getRequest()->request->all('Enseignant')['username']);
-            $user->setRoles($context->getRequest()->request->all('Enseignant')['roles']);
-
-            $usersRepository = $this->entityManager->getRepository(Users::class);
-            $usersRepository->save($user, true);
-
-
-            $this->updateEntity($this->container->get('doctrine')->getManagerForClass($context->getEntity()->getFqcn()), $entityInstance);
-
-            $this->container->get('event_dispatcher')->dispatch(new AfterEntityUpdatedEvent($entityInstance));
-
-            return $this->getRedirectResponseAfterSave($context, Action::EDIT);
-        }
-
-        $responseParameters = $this->configureResponseParameters(KeyValueStore::new([
-            'pageName' => Crud::PAGE_EDIT,
-            'templateName' => 'crud/edit',
-            'edit_form' => $editForm,
-            'entity' => $context->getEntity(),
-        ]));
-
-        $event = new AfterCrudActionEvent($context, $responseParameters);
-        $this->container->get('event_dispatcher')->dispatch($event);
-        if ($event->isPropagationStopped()) {
-            return $event->getResponse();
-        }
-
-        return $responseParameters;
-    }
-
+    // +++ On laisse EasyAdmin gérer la persistance de l'Enseignant
+    // mais on empêche la double persistance
     public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
     }
 }
