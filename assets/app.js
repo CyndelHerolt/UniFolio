c8fae76cf495561975f3689674ce0f7f60aae053
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2026-05-30 20:07:32 +0200

    fix: double initialisation de stimulus

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,13 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/
 ));
-
-import LiveController from '@symfony/ux-live-component';
-import '@symfony/ux-live-component/dist/live.min.css';
-
-app.register('live', LiveController);
